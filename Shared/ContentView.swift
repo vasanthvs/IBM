e12ab11187a1eaf046566c4832ef29e36cdd7801
vasanth <vasanthanml@gmail.com>
2022-02-15 19:23:52 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Vasanth VS on 10/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        Text("Good")
+                .fontWeight(.bold)
+              
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        
+    }
+}
---
+++ Shared/IBMApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBMApp.swift
+//  Shared
+//
+//  Created by Vasanth VS on 10/02/22.
+//
+
+import SwiftUI
+
+@main
+struct IBMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
